61d891da90948fb5c5a8024e9934ce01a64ca5d2
Sam <sam.mocabee@gmail.com>
2023-11-16 22:03:17 -0700

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import CountButton from "./CountButton"
+export default function Page(){
+    return <div>
+        <CountButton />
+        <hr />
+        <a href="https://github.com/SamuelMocabee/CSCI331_nextjs">My github repo</a>
+        <hr />
+        <p>Search Bar</p>
+    </div>
+}
