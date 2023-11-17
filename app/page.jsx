40ea4bddef8c3df81d3c29d72782d8dde199042d
Sam <sam.mocabee@gmail.com>
2023-11-16 22:19:06 -0700

    initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,13 @@
 "use client"
 import CountButton from "./CountButton"
+import CountButton2 from "./CountButton2"
+import React from "react"
 export default function Page(){
     return <div>
-        <CountButton />
+        <h1>This button will increment by 1</h1>
+        <CountButton color="blue"/>
+        <h1>This button will increment by 2</h1>
+        <CountButton2 />
         <hr />
         <a href="https://github.com/SamuelMocabee/CSCI331_nextjs">My github repo</a>
         <hr />
