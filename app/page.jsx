04b46c6a0e6e24c2a141cad3c01b8a03b6be3a62
Sam <sam.mocabee@gmail.com>
2023-11-17 01:53:32 -0700

    finalcommit

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,19 @@
 "use client"
 import CountButton from "./CountButton"
 import CountButton2 from "./CountButton2"
+import SearchBar from "./SearchBar"
+import React, {useEffect, useState} from "react"
 export default function Page(){
     return <div>
-        <h1>This button will increment by 1</h1>
+        <h1>NextJS Assignment</h1>
+        <h2>This button will increment by 1</h2>
         <CountButton color="blue"/>
-        <h1>This button will increment by 2</h1>
+        <h2>This button will increment by 2</h2>
         <CountButton2 />
         <hr />
         <a href="https://github.com/SamuelMocabee/CSCI331_nextjs">My github repo</a>
         <hr />
-        <p>Search Bar</p>
+        <SearchBar />
+        
     </div>
 }
